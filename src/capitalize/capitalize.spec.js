8c99fd68f0b7c2a96a3b5cfc1dc9ca44404672a3
Esa <esa-pitkanen@hotmail.com>
2025-10-04 11:36:37 +0300

    test(capitalize): add edge case tests for empty strings

--- src/capitalize/capitalize.spec.js
+++ src/capitalize/capitalize.spec.js
@@ -8,4 +8,8 @@ describe("Capitalize function", () => {
   test("accepts another sting", () => {
     expect(capitalize("greetings!")).toBe("Greetings!");
   });
+
+  test("accepts empty strings", () => {
+    expect(capitalize("")).toBe("");
+  });
 });

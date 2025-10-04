d6f8cea76246b2f72b79fc803d7dcd6efa4e529e
Esa <esa-pitkanen@hotmail.com>
2025-10-04 12:50:22 +0300

    fix(capitalize): throw error on invalid input

--- src/capitalize/capitalize.js
+++ src/capitalize/capitalize.js
@@ -1,4 +1,9 @@
 export function capitalize(string) {
+  if (typeof string !== "string") {
+    throw new Error("Only strings are accepted");
+  }
+
   if (string.length === 0) return "";
+  
   return string[0].toUpperCase() + string.slice(1);
 }

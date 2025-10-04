ff1dc4dba76915dc059074f598b5441eb07c24d0
Esa <esa-pitkanen@hotmail.com>
2025-10-04 19:06:39 +0300

    test(calculator): add test for floating point addition

--- src/calculator/calculator.spec.js
+++ src/calculator/calculator.spec.js
@@ -2,13 +2,16 @@ import { calculator } from "./calculator.js";
 
 describe("Calculator", () => {
   describe("add method", () => {
-    test("adds two number", () => {
+    test("adds two numbers", () => {
       expect(calculator.add(2, 3)).toBe(5);
     });
 
-    test("adds another two number", () => {
+    test("adds another two numbers", () => {
       expect(calculator.add(323, 241)).toBe(564);
     });
 
+    test("adds floating numbers", () => {
+      expect(calculator.add(0.5, 0.3)).toBeCloseTo(0.8);
+    });
   });
 });

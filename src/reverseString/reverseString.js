5a62986411700e0195834785092a440566f669b8
Esa <esa-pitkanen@hotmail.com>
2025-10-04 16:58:11 +0300

    fix(reverseString): throw error on invalid input

--- src/reverseString/reverseString.js
+++ src/reverseString/reverseString.js
@@ -1,4 +1,8 @@
 export function reverseString(string) {
+  if (typeof string !== "string") {
+    throw new Error("Only strings are accepted");
+  }
+
   let result = [];
 
   for (let i = string.length - 1; i >= 0; i--) {

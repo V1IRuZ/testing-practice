add7c22959141fd72add8cc6fc0971fd81d6a787
Esa <esa-pitkanen@hotmail.com>
2025-10-04 16:38:56 +0300

    test(reverseString): verify function reverses different strings

--- src/reverseString/reverseString.spec.js
+++ src/reverseString/reverseString.spec.js
@@ -4,4 +4,8 @@ describe("reverseString function", () => {
   test("accepts string", () => {
     expect(reverseString("hello")).toBe("olleh");
   });
+
+  test("accepts different strings", () => {
+    expect(reverseString("Greetings")).toBe("sgniteerG");
+  });
 });

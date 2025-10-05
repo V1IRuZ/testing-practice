cf0082f14ff4ae5d25e761324fca9f380ab1ca28
Esa <esa-pitkanen@hotmail.com>
2025-10-05 14:06:47 +0300

    feat(calculator): handle errors with invalid inputs in subtract method

--- src/calculator/calculator.js
+++ src/calculator/calculator.js
@@ -11,6 +11,14 @@ export const calculator = {
     return a + b;
   },
   subtract(a, b) {
+    if (
+      typeof a !== "number" ||
+      typeof b !== "number" ||
+      Number.isNaN(a) ||
+      Number.isNaN(b)
+    ) {
+      throw new Error("Only numbers are accepted");
+    }
     return a - b;
   },
   divide(a, b) {

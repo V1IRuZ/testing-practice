67adfe2f9e1139640a229d6d4047ca84b0301df5
Esa <esa-pitkanen@hotmail.com>
2025-10-05 18:23:42 +0300

    feat(caesarCipher): handle wrap-around for shifted characters

--- src/caesarCipher/caesarCipher.js
+++ src/caesarCipher/caesarCipher.js
@@ -2,10 +2,13 @@ export function caesarCipher(string, key) {
   const alphabets = "abcdefghijklmnopqrstuvwxyz";
   const message = string.split("");
 
-  const cipheredString = message.map(char => {
+  const cipheredString = message.map((char) => {
     let index = alphabets.indexOf(char) + key;
-    return alphabets[index];
-  })
+
+    const newIndex = (alphabets.length + index) % alphabets.length
+
+    return alphabets[newIndex];
+  });
 
   return cipheredString.join("");
 }

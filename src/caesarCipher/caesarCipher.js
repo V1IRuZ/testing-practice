45da550fae5a72d0b8dbf9394d4dc5e1db485bb2
Esa <esa-pitkanen@hotmail.com>
2025-10-06 17:32:37 +0300

    refactor(caesarCipher): move shift factor validation to separate function

--- src/caesarCipher/caesarCipher.js
+++ src/caesarCipher/caesarCipher.js
@@ -12,7 +12,22 @@ function cipherCharacter(character, shiftFactor) {
   return isUpperCase ? shiftedChar.toUpperCase() : shiftedChar;
 }
 
+function validateShiftFactor(shiftFactor) {
+  const minRange = 1;
+  const maxRange = 25;
+
+  if (typeof shiftFactor !== "number" || Number.isNaN(shiftFactor)) {
+    throw new Error("Shift factor must be a number.");
+  }
+
+  if (shiftFactor < minRange || shiftFactor > maxRange) {
+    throw new Error("Shift factor must be between 1 and 25.");
+  }
+}
+
 export function caesarCipher(string, shiftFactor) {
+  validateShiftFactor(shiftFactor);
+
   return string
     .split("")
     .map((char) => cipherCharacter(char, shiftFactor))

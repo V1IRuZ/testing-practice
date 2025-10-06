18ea4429555d21fe7d3bdc75fe4f99222a1efef0
Esa <esa-pitkanen@hotmail.com>
2025-10-06 15:24:54 +0300

    feat(caesarCipher): add support for uppercase letters

--- src/caesarCipher/caesarCipher.js
+++ src/caesarCipher/caesarCipher.js
@@ -1,14 +1,21 @@
 export function caesarCipher(string, key) {
   const alphabets = "abcdefghijklmnopqrstuvwxyz";
+  let upperCaseAlphabets = alphabets.toUpperCase();
   const message = string.split("");
 
   const cipheredString = message.map((char) => {
-    let index = alphabets.indexOf(char);
-    if(index === -1) return char;
+    let index;
 
-    const newIndex = (key + index) % alphabets.length
+    if (char === char.toUpperCase() && upperCaseAlphabets.includes(char)) {
+      index = alphabets.indexOf(char.toLowerCase());
+      const upperCaseIndex = (key + index) % alphabets.length;
+      return alphabets[upperCaseIndex].toUpperCase();
+    }
 
-    return alphabets[newIndex];
+    index = alphabets.indexOf(char);
+    if (index === -1) return char;
+    const lowerCaseIndex = (key + index) % alphabets.length;
+    return alphabets[lowerCaseIndex];
   });
 
   return cipheredString.join("");

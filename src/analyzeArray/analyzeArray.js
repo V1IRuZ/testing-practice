b9fa7322191177c21f5da1ed3afb1e1e9eef668c
Esa <esa-pitkanen@hotmail.com>
2025-10-07 17:53:24 +0300

    feat(analyzeArray): add min value calculation to returned object

--- src/analyzeArray/analyzeArray.js
+++ src/analyzeArray/analyzeArray.js
@@ -1,7 +1,13 @@
 export function analyzeArray(array) {
   const average = array.reduce((sum, num) => sum + num, 0) / array.length;
 
+  const min = array.reduce(
+    (lowest, current) => (lowest < current ? lowest : current),
+    array[0],
+  );
+
   return {
-    average
-  }
+    average,
+    min,
+  };
 }

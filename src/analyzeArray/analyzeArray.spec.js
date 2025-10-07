21d592a6e44608c57a6dd5a1a94c0696cfae0423
Esa <esa-pitkanen@hotmail.com>
2025-10-07 17:47:00 +0300

    test(analyzeArray): add test for the minimum value

--- src/analyzeArray/analyzeArray.spec.js
+++ src/analyzeArray/analyzeArray.spec.js
@@ -4,4 +4,8 @@ describe("analyzeArray function", () => {
   test("returns the average of an array in an object", () => {
     expect(analyzeArray([2, 3, 5, 6, 9]).average).toBe(5);
   });
+
+  test("returns the minimum value from an array", () => {
+    expect(analyzeArray([5, 7, 4, 23, 2, 9]).min).toBe(2);
+  });
 });
